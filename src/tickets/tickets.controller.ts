eee1e16b3dbb3df904611b68f1e40c69699fe893
Alfred Akintunde AMOS <52054578+alfredamos@users.noreply.github.com>
2026-05-24 14:58:40 +0100

    Add ticket service methods and controller endpoints.

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,9 +1,47 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
+import {TicketUncheckedCreateInput, TicketUncheckedUpdateInput} from "../generated/prisma/models/Ticket";
+import {Role} from "../generated/prisma/enums";
+import {Roles} from "../decorators/role.decorator";
 
 @Controller('tickets')
 export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
 
+  @Roles(Role.Admin)
+  @Post()
+  async createTicket(@Body() ticket: TicketUncheckedCreateInput) {
+    return await this.ticketsService.createTicket(ticket);
+  }
+
+  @Roles(Role.Admin)
+  @Delete(':id')
+  async deleteTicketById(@Param('id') id: string) {
+    return await this.ticketsService.deleteTicketById(id);
+  }
+
+  @Roles(Role.Admin)
+  @Patch(':id')
+  async editTicketById(@Param('id') id: string, @Body() ticket: TicketUncheckedUpdateInput) {
+    return await this.ticketsService.editTicketById(id, ticket);
+  }
+
+  @Roles(Role.Admin)
+  @Get(':id')
+  async getTicketById(@Param('id') id: string) {
+    return await this.ticketsService.getTicketById(id);
+  }
+
+  @Roles(Role.Admin)
+  @Get()
+  async getAllTickets() {
+    return await this.ticketsService.getAllTickets();
+  }
+
+  @Roles(Role.Admin)
+  @Get('by-customer-id/:customerId')
+  async getAllTicketsByCustomerId(@Param('customerId') customerId: string) {
+    return await this.ticketsService.getAllTicketsByCustomerId(customerId);
+  }
 
 }
--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,6 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {TicketUncheckedCreateInput, TicketUncheckedUpdateInput} from "../generated/prisma/models/Ticket";
+import {PrismaService} from "../prisma/prisma.service";
+import {toTicketResponse} from "../models/ticketResponse.model";
+import {TicketWithCustomer} from "../models/ticketWithCustomer";
 
 @Injectable()
 export class TicketsService {
+    constructor(private readonly prisma: PrismaService) {}
 
+    async createTicket(ticket: TicketUncheckedCreateInput) {
+        //----> Create the ticket
+        const newTicket = await this.prisma.ticket.create({data: ticket, include: {customer: {include: {user: true}}}});
+
+        //----> Send back the response.
+        return toTicketResponse(newTicket as TicketWithCustomer);
+    }
+
+    async deleteTicketById(id: string) {
+        //----> Check for ticket existence.
+        await this.getOneTicket(id);
+
+        //----> Delete the ticket.
+        const deletedTicket = await this.prisma.ticket.delete({where: {id}, include: {customer: {include: {user: true}}}});
+
+        //----> Send back the response.
+        return toTicketResponse(deletedTicket as TicketWithCustomer);
+    }
+
+    async editTicketById(id: string, ticket: TicketUncheckedUpdateInput) {
+        //----> Check for ticket existence.
+        await this.getOneTicket(id);
+
+        //----> Edit the ticket.
+        const editedTicket = await this.prisma.ticket.update({where: {id}, data: ticket, include: {customer: {include: {user: true}}}});
+
+        //----> Send back the response.
+        return toTicketResponse(editedTicket as TicketWithCustomer);
+    }
+
+    async getTicketById(id: string) {
+        //----> Get the ticket by id.
+        const ticket = await this.getOneTicket(id);
+
+        //----> Send back response.
+        return toTicketResponse(ticket as TicketWithCustomer);
+    }
+
+    async getAllTickets() {
+        //----> Fetch all tickets.
+        const tickets = await this.prisma.ticket.findMany({include: {customer: {include: {user: true}}}});
+
+        //----> Send back response.
+        return tickets?.map(ticket => toTicketResponse(ticket as TicketWithCustomer));
+    }
+
+    async getAllTicketsByCustomerId(customerId: string) {
+        //----> Fetch all tickets by customer id.
+        const tickets = await this.prisma.ticket.findMany({where: {customerId}, include: {customer: {include: {user: true}}}});
+
+        //----> Send back response.
+        return tickets?.map(ticket => toTicketResponse(ticket as TicketWithCustomer));
+    }
+
+    private async getOneTicket(id: string) {
+        //----> Get the ticket by id
+        const ticket = await this.prisma.ticket.findUnique({where: {id}, include: {customer: {include: {user: true}}}});
+
+        //----> Check for null ticket.
+        if (!ticket) {
+            throw new NotFoundException("Ticket not found in db!");
+        }
+
+        //----> Return the ticket.
+        return ticket;
+    }
 }
